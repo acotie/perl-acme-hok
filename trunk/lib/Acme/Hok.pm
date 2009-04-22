110d0c71e194e0df532d2a81fd6d24f88663fe5a
Akiko Yokoyama <acotie.acotie@gmail.com>
2009-04-23 04:14:49 +0900

    message

---
+++ trunk/lib/Acme/Hok.pm
@@ -0,0 +1,33 @@
+package Acme::Hok;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Acme::Hok -
+
+=head1 SYNOPSIS
+
+  use Acme::Hok;
+
+=head1 DESCRIPTION
+
+Acme::Hok is
+
+=head1 AUTHOR
+
+Akiko Yokoyama E<lt>info {at} poccori.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
